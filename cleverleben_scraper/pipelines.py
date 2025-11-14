96babeea8b111c71eec57d4d8b1673921aaa2f13
Jigil-ak <jigil.ak2003@gmail.com>
2025-11-14 14:16:10 +0000

    Added and activated cleaning pipeline

--- cleverleben_scraper/pipelines.py
+++ cleverleben_scraper/pipelines.py
@@ -1,13 +1,45 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
+# This code follows PEP8 standards
+import re
 
 class CleverlebenScraperPipeline:
+    """
+    This pipeline cleans the data for each item.
+    - Cleans product_description to remove extra whitespace.
+    - Converts price from string to a float.
+    - Ensures image is a list.
+    """
+
     def process_item(self, item, spider):
+        
+        # --- Clean product_description ---
+        # It's a string, so we can clean it.
+        description = item.get('product_description')
+        if description:
+            # Join all parts (if it's a list) and strip whitespace
+            clean_desc = " ".join(str(description).split()).strip()
+            item['product_description'] = clean_desc
+
+        # --- Clean Price ---
+        # The API gives price as a number (e.g., 2.49), so it's good.
+        # But if it were a string "2,49", we would clean it like this:
+        price = item.get('price')
+        if price:
+            try:
+                # Convert price to a floating-point number (decimal)
+                item['price'] = float(price)
+            except ValueError:
+                # If conversion fails, keep the original
+                pass 
+
+        # --- Clean Image ---
+        # The PDF requires 'image' to be a list [cite: 161]
+        image_list = item.get('image')
+        if image_list:
+            # Our spider already makes it a list, but this is a good check.
+            # We also remove any empty or null links from the list.
+            item['image'] = [url for url in image_list if url]
+        else:
+            # If no image, set it to an empty list
+            item['image'] = []
+
         return item
--- cleverleben_scraper/settings.py
+++ cleverleben_scraper/settings.py
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "cleverleben_scraper.pipelines.CleverlebenScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "cleverleben_scraper.pipelines.CleverlebenScraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
